Really fixed weight visualization overflow

toy_data/cov_shift.py
@@ -114,6 +114,8 @@ def visualize_2D_classification(data, classifyF=None, res=150, fig_width=500):
                 _y_hats_xy = y_hats.reshape((res, res)) / 4
             else:
                 _y_hats_xy = (1 - y_hats).reshape((res, res)) / 4
+            if _y_hats_xy.any() > 1 or _y_hats_xy.any() < 0:
+                _y_hats_xy = min_max_norm(_y_hats_xy)
             _c_col = np.tile(data.colors[_c], (res, res, 1))
             _rgba = np.dstack((_c_col, np.round(255 * _y_hats_xy))).astype(np.uint8)
             img = np.squeeze(_rgba.view(np.uint32))
@@ -159,7 +161,7 @@ def visualize_2D_classification_with_tr_weights(data, weights, classifyF=None, r
                 _y_hats_xy = y_hats.reshape((res, res)) / 5
             else:
                 _y_hats_xy = (1 - y_hats).reshape((res, res)) / 5
-            if any(_y_hats_xy > 1) or any(_y_hats_xy < 0):
+            if _y_hats_xy.any() > 1 or _y_hats_xy.any() < 0:
                 _y_hats_xy = min_max_norm(_y_hats_xy)
             _c_col = np.tile(data.colors[_c], (res, res, 1))
             _rgba = np.dstack((_c_col, np.round(255 * _y_hats_xy))).astype(np.uint8)

toy_data/test/test.py
@@ -31,16 +31,16 @@ if False:
     print(l.y)
 
 if COV_SHIFT:
-    the_model = m.Sine()
+    the_model = m.Sine_1D()
     cs = cov_shift.Gaussian_Shift_1D(model=the_model)
     # cov_shift.visualize_1D_regression(cs)
     cov_shift.visualize_1D_regression_with_tr_weights(cs, cs.tr.X)
     the_model = m.LinearBinary(2)
     cs = cov_shift.Gaussian_Shift_2D_BinaryClassification(model=the_model)
-    cov_shift.visualize_2D_classification(cs, classifyF=lambda x: x[:, 0] + x[:, 1] > 0)
-    # cov_shift.visualize_2D_classification_with_tr_weights(cs,
-    #                                                      np.ravel(np.random.random(cs.tr.y.shape)),
-    #                                                      classifyF=lambda x: x[:, 0] + x[:, 1] > 0)
+    # cov_shift.visualize_2D_classification(cs, classifyF=lambda x: x[:, 0] + x[:, 1] > 0)
+    cov_shift.visualize_2D_classification_with_tr_weights(cs,
+                                                         np.ravel(np.random.random(cs.tr.y.shape)),
+                                                         classifyF=lambda x: x[:, 0] + x[:, 1] > 0)
     # cov_shift.visualize_2D_classification_with_tr_weights(cs,
     #                                                      np.ravel(np.random.random(cs.tr.y.shape)))
 
@@ -50,6 +50,4 @@ if MODEL:
     print(r)
     cs = cov_shift.Gaussian_Shift_2D_BinaryClassification(model=the_model)
     # cov_shift.visualize_2D_classification(cs, classifyF=lambda x: x[:, 0] + x[:, 1] > 0)
-    cov_shift.visualize_2D_classification(cs, classifyF=the_model)
-
-
+    # cov_shift.visualize_2D_classification(cs, classifyF=the_model)